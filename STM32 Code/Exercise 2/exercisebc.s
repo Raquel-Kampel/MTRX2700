d4fb49c983358d18093bcca23e31ee553560f27a
EricjJiang <160563214+EricjJiang@users.noreply.github.com>
2024-03-18 13:37:19 +1100

    Update exercisebc.s finished and working

--- STM32 Code/Exercise 2/exercisebc.s
+++ STM32 Code/Exercise 2/exercisebc.s
@@ -4,6 +4,8 @@
 #include "definitions.s"
 
 	//Question 2.b & c
+	LDR R3, =#0b00000000
+
 wait_for_button:
 
 @	Wait for button:
@@ -14,26 +16,21 @@ wait_for_button:
 	B wait_for_button
 
 button_pressed:
-	LDR R0, =GPIOE
 	@check if all the leds are on
 	LDR R5, =#0b10000000
 	CMP R3, R5
 	BEQ reset
 	BMI turn_on
 	@turn off
-	LSLS R3, R3, #0
-	ORR R3, R3, #0
+	LSLS R3, R3, #1
 	LDR R0, =GPIOE
 	STRB R3, [R0, #ODR + 1]
-	ADD R6, #1
 	B wait_for_button
 
 reset:
-	LSLS R3, R3, #0
-	ORR R3, R3, #0
+	LSLS R3, R3, #1
 	LDR R0, =GPIOE
 	STRB R3, [R0, #ODR + 1]
-	LDR R6, =0
 	B wait_for_button
 
 
@@ -42,5 +39,4 @@ turn_on:
 	ORR R3, R3, #1
 	LDR R0, =GPIOE
 	STRB R3, [R0, #ODR + 1]
-	ADD R6, #1
 	B wait_for_button
