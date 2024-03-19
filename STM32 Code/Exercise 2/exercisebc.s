1cb59d09e24dd9aa51945b8191aae96c2b002ea7
EricjJiang <160563214+EricjJiang@users.noreply.github.com>
2024-03-19 11:14:24 +1100

    Update exercisebc.s

--- STM32 Code/Exercise 2/exercisebc.s
+++ STM32 Code/Exercise 2/exercisebc.s
@@ -3,21 +3,18 @@
 
 #include "definitions.s"
 
-	//Question 2.b & c
-	LDR R3, =#0b00000000
-
 main_2bc:
 
-@	Wait for button:
-	LDR R0, =GPIOA
-	LDRB R1, [R0, #IDR]
-	ANDS R1, #0x01
-	BNE button_pressed
-	B wait_for_button
+	@Wait for the user input button to be pressed to go to the next command
+	LDR R0, =GPIOA @load the address of the GPIOE register into R0
+	LDRB R1, [R0, #IDR] @store this to the second byte of the ODR (bits 8-15) into R1
+	ANDS R1, #0x01 @check if the button is pressed
+	BNE button_pressed @branch to the next command
+	B wait_for_button @loop if button is not pressed
 
 button_pressed:
-	@check if all the leds are on
-	LDR R5, =#0b10000000
+	@check if all the LEDs are on
+	LDR R5, =#0b10000000 @
 	CMP R3, R5
 	BEQ reset
 	BMI turn_on
