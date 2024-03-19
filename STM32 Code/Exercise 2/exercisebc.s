8bf8c1d0de91ebb9c2021c83456c7ca3ea51342c
EricjJiang <160563214+EricjJiang@users.noreply.github.com>
2024-03-19 12:19:15 +1100

    Update exercisebc.s

--- STM32 Code/Exercise 2/exercisebc.s
+++ STM32 Code/Exercise 2/exercisebc.s
@@ -15,32 +15,33 @@ main_2bc:
 button_pressed:
 	@check if all the leds are on
 	LDR R3, =#0b10000000 @bitmask of only the last led is on
-	CMP R2, R3 @
+	CMP R2, R3 @if R2 > R3 --> a led should be turned on, else a led should be turned off
 	BEQ reset @function that turns off the last led 
-	BMI turn_on
-	@turn off
-	LSLS R2, R2, #1
+	BMI turn_on @
+	
+ 	@when R2 < R3, turn off a led
+	LSLS R2, R2, #1 @shifts the bitmask to the left 
+	SUB R2, #128 @get rid of the overflow 9th binary bit
 	SUB R2, #128
-	SUB R2, #128
-	LDR R0, =GPIOE
+	LDR R0, =GPIOE @store the new bitmask into the GPIOE
 	STRB R2, [R0, #ODR + 1]
 	B delay_function
 
 reset:
-	LDR R2, =#0b00000000
-	LDR R0, =GPIOE
+	LDR R2, =#0b00000000 @turn last led off
+	LDR R0, =GPIOE @store the new bitmask into the GPIOE
 	STRB R2, [R0, #ODR + 1]
 	B delay_function
 
 
 turn_on:
-	LSLS R2, R2, #1
-	ORR R2, R2, #1
-	LDR R0, =GPIOE
+	LSLS R2, R2, #1 @shift to the left by 1
+	ORR R2, R2, #1 @change the lowest bit to 1
+	LDR R0, =GPIOE @store the new bitmask into the GPIOE
 	STRB R2, [R0, #ODR + 1]
 	B delay_function
 
-delay_function:
+delay_function: @delay the led and button pressing 
 	LDR R4, =#0x7A120
 	BL delay_loop
 
