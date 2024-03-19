5b8ab03f2d9a457ec36c37f9343d51d32b9a3a73
EricjJiang <160563214+EricjJiang@users.noreply.github.com>
2024-03-19 13:35:08 +1100

    Update assembly.s

--- STM32 Code/Exercise 2/assembly.s
+++ STM32 Code/Exercise 2/assembly.s
@@ -12,7 +12,7 @@
 .data
 
 @ Define variables for Question 2.d, bitmask that is needed to count the number of a letter
-ascii_string: .asciz "aaabbddddfllloozzzzzzz\0" 
+ascii_string: .asciz "aaabbddddfllloozzzzzzz\0"
 
 
 .text
@@ -27,24 +27,21 @@ main:
 
 @Exercise 2a, turn LEDs on with bitmask pattern
 
- 	/*
+/*
 	LDR R1, =0b10101010 @store the current light pattern (binary mask) in R1
 	BL main_2a @branch to the function for exercise 2a
- 	*/
+*/
 
 @Exercise 2b and 2c, turns LEDs on/off with the user input button
 
- 	/*
+/*
 	LDR R2, =#0b00000000 @stores the original bitmask of the leds into r2
 	BL main_2bc @branch to the function for exercise 2b and 2c
- 	*/
+*/
 
 @Exercise 2d
 
-	/*
- 	BL main_2d branch to the function for exercise 2d
-  	*/
+/*
+ 	BL main_2d @branch to the function for exercise 2d
+*/
 
-finished_everything:
-
-	B finished_everything 	@ infinite loop here
