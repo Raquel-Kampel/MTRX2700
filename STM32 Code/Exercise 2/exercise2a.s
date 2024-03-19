2f5eb6e34df282a8f3dee81c4ffb67b900ce395d
EricjJiang <160563214+EricjJiang@users.noreply.github.com>
2024-03-19 12:11:26 +1100

    Update exercise2a.s

--- STM32 Code/Exercise 2/exercise2a.s
+++ STM32 Code/Exercise 2/exercise2a.s
@@ -5,6 +5,6 @@
 
 main_2a:
 	LDR R0, =GPIOE  @load the address of the GPIOE register into R0
-	STRB R4, [R0, #ODR + 1]   @store the bitmask to the second byte of the ODR (bits 8-15)
+	STRB R1, [R0, #ODR + 1]   @store the bitmask to the second byte of the ODR (bits 8-15)
 
-	B main_2a @return to the program_loop label
+	B main_2a @loops the function
